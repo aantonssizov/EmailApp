d5163f55ccb4ed4c7dff5008ec6f04b7560a869a
Anton Sizov <aantonssizov@gmail.com>
2019-02-10 23:00:05 +0200

    I come sleep

--- app/src/main/java/com/example/emailapp/MainActivity.java
+++ app/src/main/java/com/example/emailapp/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.emailapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView ribbonOfEmails;
+    private RecyclerView.Adapter ribbonOfEmailsAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ribbonOfEmails = (RecyclerView) findViewById(R.id.ribbon_of_emails);
+
+
     }
 }
